634301c2052f6e720ef27312c2ed5ac3d17e36fe
takforra0312 <ec2-user@ip-172-31-46-12.us-east-2.compute.internal>
2020-05-24 14:11:11 +0000

    adding_some_function

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200509081707) do
+ActiveRecord::Schema.define(version: 2020_05_22_154551) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name", unique: true
   end
 
 end
